487a313bf3348afa7423911efb609e92f81cf518
jack <jackkaback@gmail.com>
2018-12-18 16:10:00 -0500

    implemented update func

--- src/life.java
+++ src/life.java
@@ -21,13 +21,16 @@ public class life {
 		initializeArea(field, x, y);
 
 		printBoard(field, x, y);
+		System.out.println("------------------------------------------------------");
+		updateBoard(field, x, y);
+		printBoard(field, x, y);
 
 	}
 
 	private static int getval(int val, String name, Scanner keyin){
 
 		while(true) {
-			System.out.println("Enter the " + name +" of the area");
+			System.out.print("Enter the " + name +" of the area");
 			String temp = keyin.nextLine();
 			if (isNumeric(temp)) {
 				val = Integer.parseInt(temp);
@@ -41,19 +44,17 @@ public class life {
 	private static void initializeArea(boolean[][] f, int x, int y){
 		for(int ii = 0; ii < x; ii++){
 			for(int jj = 0; jj < y; jj++){
-				f[ii][jj] = false;
+				f[ii][jj] = true;
 			}
 		}
 	}
 
 	private static boolean isNumeric(String checkString) {
-		int ii = 0;
-
 		if (checkString == null) {
 			return false;
 		}
 
-		for ( ;ii < checkString.length(); ii++) {
+		for (int ii = 0; ii < checkString.length(); ii++) {
 			if (checkString.charAt(ii) < '0' || checkString.charAt(ii) > '9') { //this checks if the input is only numbers
 				return false;
 			}
@@ -72,6 +73,57 @@ public class life {
 				}
 			}
 			System.out.println();
+		}
+	}
+
+	private static void updateBoard(boolean[][] f, int x, int y){
+		boolean temp[][] = f;
+
+		for (int ii = 1; ii < x - 1; ii++){
+			for (int jj = 1; jj < y - 1; jj++){
+				int count = 0;
+
+				//above
+				if(temp[ii-1][jj-1]){
+					count++;
+				}
+				if(temp[ii][jj-1]){
+					count++;
+				}
+				if(temp[ii+1][jj-1]){
+					count++;
+				}
+
+				//sides
+				if(temp[ii-1][jj]){
+					count++;
+				}if(f[ii+1][jj]){
+					count++;
+				}
+
+				//below
+				if(temp[ii-1][jj+1]){
+					count++;
+				}
+				if(temp[ii][jj+1]){
+					count++;
+				}
+				if(temp[ii+1][jj+1]){
+					count++;
+				}
+
+
+				//updating
+				if(temp[ii][jj] && (count < 2 || count > 3)){
+					f[ii][jj] = false;
+				}
+				else if(!temp[ii][jj] && count == 3){
+					f[ii][jj] = true;
+				}
+				else if(temp[ii][jj] && (count == 2 || count == 3)){
+					f[ii][jj] = true;
+				}
+			}
 		}
 	}
 }
